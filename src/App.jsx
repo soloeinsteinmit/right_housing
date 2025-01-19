9636d0982fb52072e5c563457e5d9b1cf608e394
Solomon Eshun <Ssolomoneinsteinmit300@gmail.com>
2025-01-19 03:52:30 +0000

    initial commit: setup project(heroui and tailwindcss)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "@heroui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+      <h1 class="text-6xl font-bold">Right Housing</h1>
+      <Button color="primary" size="lg">
+        Right Housing
+      </Button>
+    </div>
+  );
+}
+
+export default App;
+//
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+import { heroui } from "@heroui/theme";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
